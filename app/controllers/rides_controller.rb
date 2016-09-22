c7719e98c7232f2cf42cb7b1a2e0415381a19d3a
karuna24s <karuna24s@gmail.com>
2016-09-21 20:12:43 -0400

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,12 +1,12 @@
 class RidesController < ApplicationController
+  def new
+    @ride = Ride.new
+  end
 
   def create
-    @ride = Ride.new(ride_params)
-    @user = User.find_by(params[:user_id])
-    if @ride.save
-      flash[:notice] = @ride.take_ride
-      redirect_to user_path(@user)
-    end
+    @ride = Ride.create(ride_params)
+    @alert = @ride.take_ride
+    redirect_to user_path(@ride.user), alert: @alert
   end
 
   private
